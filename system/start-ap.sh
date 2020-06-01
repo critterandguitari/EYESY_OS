6d2ef8d679af945e8df72371869f13c0ecf06b78
Owen Osborn <owenosborn@gmail.com>
2020-06-01 10:41:58 -0400

    adding ap stuff

---
+++ system/start-ap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+systemctl stop wpa_supplicant
+systemctl stop dhcpcd
+
+AP_FILE="/sdcard/ap.txt"
+
+if [ -f "$AP_FILE" ]; then
+    echo "$AP_FILE exists"
+    NET=$(head -n 1 $AP_FILE)
+    PW=$(tail -n 1 $AP_FILE)
+else 
+    echo "$AP_FILE does not exist, using default"
+    NET=EYESY
+    PW=coolmusic
+fi
+
+/home/music/EYESY_OS/system/create_ap --no-virt -n wlan0 $NET $PW
+

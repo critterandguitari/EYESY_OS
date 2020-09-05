8605eb05c67fe1422c1e37c7092351af3e6872a9
Owen Osborn <owenosborn@gmail.com>
2020-09-05 17:40:18 -0400

    remove eyesy of from here for now

--- engines/oflua/run.sh
+++ engines/oflua/run.sh
@@ -4,5 +4,5 @@ export LUA_PATH="/home/music/EYESY_OS/engines/oflua/?.lua;;"
 # make sure log ownership is not root
 sudo chown music:music /tmp/video.log
 
-cd /home/music/EYESY_OS/engines/oflua/eyesy
+cd /home/music/openFrameworks/apps/myApps/eyesy
 stdbuf -o0 bin/eyesy &> /tmp/video.log

89c8f0335aadf44c84e2fced39d80fef1d4f5f8c
Owen Osborn <owenosborn@gmail.com>
2022-08-08 10:38:58 -0400

    remove old systemd services

--- platforms/eyesy_cm3/deploy.sh
+++ platforms/eyesy_cm3/deploy.sh
@@ -13,16 +13,16 @@ sync
 mkdir /sdcard/System
 
 # configure systemd stuff
-systemctl disable eyesy-oflua.service  
 systemctl enable cherrypy.service  
 systemctl enable splashscreen.service  
 systemctl enable ttymidi.service  
 
 # disable old way of starting things
 systemctl disable eyesy-pd.service  
+systemctl disable eyesy-oflua.service  
 systemctl disable eyesy-python.service  
 
-# networking started by eyesy-pd
+# networking started manually 
 systemctl disable dhcpcd.service
 systemctl disable wpa_supplicant.service
 systemctl disable createap.service  

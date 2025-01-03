8237fff02900cfdf1764698139d866d00a8b4af7
Owen Osborn <owen@critterandguitari.com>
2025-01-02 23:02:35 -0500

    clean up, add fgbg palette visual

--- engines/python/screen_palette.py
+++ engines/python/screen_palette.py
@@ -5,40 +5,38 @@ from widget_menu import WidgetMenu, MenuItem
 import pygame
 
 def draw_color_palette(surface, app_state):
-
-    width, height = 50, 300  # Dimensions of the preview (swapped for vertical gradient)
+    
+    # bg
+    width, height = 260, 260  
     xoff = 350
-    yoff = 50
+    yoff = 100
+    for i in range(height):
+        # Get the color using the color_picker function
+        color = app_state.color_picker_bg(i / height)
+        # Draw a horizontal line (1 pixel high)
+        pygame.draw.line(surface, color, (xoff, i + yoff), (width - 1 + xoff, i + yoff))
+
+    # fg
+    width, height = 160, 160  
+    xoff = 420
+    yoff = 170
     for i in range(height):
         # Get the color using the color_picker function
         color = app_state.color_picker(i / height)
         # Draw a horizontal line (1 pixel high)
         pygame.draw.line(surface, color, (xoff, i + yoff), (width - 1 + xoff, i + yoff))
 
-# Function to handle palette selection, TODO is this really necessary to set them since they get set with each selection anyway?
-def select_fg_palette(app_state, index):
-    app_state.fg_palette = index
-    print(f"Selected fg palette: {app_state.palettes[index]['name']}")
 
-def select_bg_palette(app_state, index):
-    app_state.bg_palette = index
-    print(f"Selected bg palette: {app_state.palettes[index]['name']}")
+# Function to handle palette selection, not really necessary to set them since they get set with each selection anyway?
+def select_palette(app_state, index):
+    pass
 
 # Function to dynamically create a menu based on palettes
-def create_fg_palette_menu(app_state):
+def create_palette_menu(app_state):
     menu_items = []
     for i, palette in enumerate(app_state.palettes):
         # Create a menu item for each palette
-        menu_items.append(MenuItem(palette['name'], lambda i=i: select_fg_palette(app_state, i)))
-
-    # Return the menu object
-    return WidgetMenu(app_state, menu_items)
-
-def create_bg_palette_menu(app_state):
-    menu_items = []
-    for i, palette in enumerate(app_state.palettes):
-        # Create a menu item for each palette
-        menu_items.append(MenuItem(palette['name'], lambda i=i: select_bg_palette(app_state, i)))
+        menu_items.append(MenuItem(palette['name'], lambda i=i: select_palette(app_state, i)))
 
     # Return the menu object
     return WidgetMenu(app_state, menu_items)
@@ -47,8 +45,8 @@ class ScreenPalette(Screen):
     def __init__(self, app_state):
         super().__init__(app_state)
         self.title = "Color Palette"
-        self.fg_menu = create_fg_palette_menu(app_state)
-        self.bg_menu = create_bg_palette_menu(app_state)
+        self.fg_menu = create_palette_menu(app_state)
+        self.bg_menu = create_palette_menu(app_state)
         self.fg_menu.off_y = 50
         self.bg_menu.off_y = 250
         #self.menu.items.append(MenuItem('◀ Exit', self.goto_home))
@@ -67,7 +65,6 @@ class ScreenPalette(Screen):
         draw_color_palette(surface, self.app_state)
 
     def before(self):
-        print("entering color palette")
         self.fg_menu.set_selected_index(self.app_state.fg_palette)
         self.bg_menu.set_selected_index(self.app_state.bg_palette)
 

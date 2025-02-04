f7b0dfd9e7deb25d04108ebf973b18a90b16dd52
CG <example@example.com>
2025-02-03 21:46:59 -0500

    fix composite video

--- platforms/eyesy_cm3/rootfs/etc/eyesysetup.sh
+++ platforms/eyesy_cm3/rootfs/etc/eyesysetup.sh
@@ -6,6 +6,14 @@ echo -n performance | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_gove
 # disable status led after bootup
 #echo none > /sys/class/leds/led0/trigger
 
+# Check HDMI status using kmsprint
+if kmsprint | grep -q "HDMI-A-1 (connected)"; then
+    echo "HDMI is connected, skipping composite activation."
+else
+    echo "HDMI not connected, enabling composite video."
+    echo "on" | sudo tee /sys/class/drm/card0-Composite-1/status
+fi
+
 # redirect 80 to 8080 for the web server
 iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 8080
 
